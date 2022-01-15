e245f2017d479055fc35abeb9c5dd1598c9fdca8
JACOBDUCAS <jducas182@gmail.com>
2022-01-15 15:23:42 -0500

    Create spotifyApi.py
    Réussi faire playlist + ajouter chansons

---
+++ spotifyApi.py
@@ -0,0 +1,29 @@
+import spotipy
+from spotipy.oauth2 import SpotifyOAuth
+import json
+
+scope='playlist-modify-public'
+username='tctib79y7zvz39xim0jdvxcst'
+
+token = SpotifyOAuth(scope=scope,username=username)
+spotifyObject = spotipy.Spotify(auth_manager=token)
+
+#creer playlist
+playlist_name = input('Nom playlist: ')
+playlist_desc = 'New songs from reddit'
+
+spotifyObject.user_playlist_create(user=username,name=playlist_name,public=True,description=playlist_desc)
+
+user_input = input('Enter the song: ')
+list_songs = []
+while user_input !='quit':
+    result = spotifyObject.search(q=user_input)
+    #print(json.dumps(result, sort_keys=4, indent=4))
+    list_songs.append(result['tracks']['items'][0]['uri'])
+    user_input = input('Enter the song: ')
+
+prePlaylist = spotifyObject.user_playlists(user=username)
+playlist = prePlaylist['items'][0]['id']
+
+
+spotifyObject.user_playlist_add_tracks(user=username,playlist_id=playlist,tracks=list_songs)
